1bee9701f6fec2d2ce125e927fb7ac3a5416b65f
neel20409 <bhattneel2004@gmail.com>
2026-07-09 12:58:58 +0530

    Server error fix

--- dist/prisma/seed.js
+++ dist/prisma/seed.js
@@ -34,8 +34,16 @@ var __importStar = (this && this.__importStar) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
 const bcrypt = __importStar(require("bcryptjs"));
-const prisma = new client_1.PrismaClient();
+const pg_1 = require("pg");
+require("dotenv/config");
+const pool = new pg_1.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
+const adapter = new adapter_pg_1.PrismaPg(pool);
+const prisma = new client_1.PrismaClient({ adapter });
 async function main() {
     console.log('🌱 Seeding RentFlow database (Indian Localized)...\n');
     console.log('🧹 Cleaning existing data...');
--- dist/src/prisma/prisma.service.js
+++ dist/src/prisma/prisma.service.js
@@ -12,9 +12,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PrismaService = void 0;
 const common_1 = require("@nestjs/common");
 const client_1 = require("@prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const pg_1 = require("pg");
 let PrismaService = class PrismaService extends client_1.PrismaClient {
     constructor() {
-        super();
+        const pool = new pg_1.Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        });
+        const adapter = new adapter_pg_1.PrismaPg(pool);
+        super({ adapter });
     }
     async onModuleInit() {
         await this.$connect();
